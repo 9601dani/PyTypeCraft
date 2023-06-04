1dcaadb7b4449b381947336cdf42103a14475626
9601dani <erickmorales201930699@cunoc.edu.gt>
2023-06-04 17:27:06 -0600

    api recibe informacion

---
+++ BackendCraft/BackendPyCraft/api_rest.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def test_api():
+    return {"Hello": "from test_api"}
+@app.get("/compiler/{text}")
+def get_text_compiler():
+    return {"text from compiler/texto"}
