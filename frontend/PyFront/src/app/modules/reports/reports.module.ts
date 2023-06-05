0a4a0d66eaed034c85aa11bd4f8cd71c9f339afa
Levi <levihernandez2501@gmail.com>
2023-06-04 23:39:24 -0600

    Mejorando vista Análisis, creando vista Reportes

--- frontend/PyFront/src/app/modules/reports/reports.module.ts
+++ frontend/PyFront/src/app/modules/reports/reports.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { ReportsRoutingModule } from './reports-routing.module';
 import { ReportPageComponent } from './pages/report-page/report-page.component';
+import {SharedModule} from "../../shared/shared.module";
 
 
 @NgModule({
   declarations: [
     ReportPageComponent
   ],
-  imports: [
-    CommonModule,
-    ReportsRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        ReportsRoutingModule,
+        SharedModule
+    ]
 })
 export class ReportsModule { }
