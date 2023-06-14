74b0a3a9c6d2bce3992fccf8c24a1eaa9916fbc2
Levi <levihernandez2501@gmail.com>
2023-06-13 19:56:22 -0600

    Implementando comportamiento de if, while, asignaciones en Debugger.py, arreglando error en SymbolTable.py

--- BackendCraft/BackendPyCraft/src/symbolTable/SymbolTable.py
+++ BackendCraft/BackendPyCraft/src/symbolTable/SymbolTable.py
@@ -1,6 +1,7 @@
 from ..models.SymbolType import SymbolType
 from ..models.Variable import Variable
 
+
 class SymbolTable:
 
     def __init__(self, parent=None):
@@ -16,22 +17,22 @@ class SymbolTable:
     # THIS METHOD CHECKS IF THE ID VARIABLE IS ALREADY DECLARED
     # IF IS DECLARED THEN RETURN IT.
     def find_var_by_id(self, id: str):
-        current_table = self
+        current_table: SymbolTable = self
 
         while current_table is not None:
 
             for symbol in current_table.symbols:
                 if symbol.symbol_type == SymbolType.VARIABLE and symbol.id == id:
-                    print("Variable encontrada: "+symbol.__str__())
+                    # print("Variable encontrada: "+symbol.__str__())
                     return symbol
 
             current_table = current_table.parent
 
         return None
 
-    #BOOLEAN METHOD, RETURN TRUE IF VARIABLE IS IN TABLE, IF NOT RETURN FALSE
+    # BOOLEAN METHOD, RETURN TRUE IF VARIABLE IS IN TABLE, IF NOT RETURN FALSE
     def var_in_table(self, id: str):
-        return any( (var.id == id and var.symbol_type == SymbolType.VARIABLE) for var in self.symbols)
+        return any((var.id == id and var.symbol_type == SymbolType.VARIABLE) for var in self.symbols)
 
     # THIS METHOD CHECKS IF THE ID FUNCTION IS ALREADY DECLARED
     # IF IS DECLARED THEN RETURN IT.
@@ -41,19 +42,19 @@ class SymbolTable:
         while current_table is not None:
             for symbol in current_table.symbols:
                 if symbol.symbol_type == SymbolType.FUNCTION and symbol.id == id:
-                    print("Variable encontrada: "+symbol.__str__())
+                    # print("Variable encontrada: "+symbol.__str__())
                     return symbol
 
             current_table = current_table.parent
 
         return None
 
-    #BOOLEAN METHOD, RETURN TRUE IF FUNCTION IS IN TABLE, IF NOT RETURN FALSE
+    # BOOLEAN METHOD, RETURN TRUE IF FUNCTION IS IN TABLE, IF NOT RETURN FALSE
     def fun_in_table(self, id: str):
-        return any( (var.id == id and var.symbol_type == SymbolType.FUNCTION) for var in self.symbols)
+        return any((var.id == id and var.symbol_type == SymbolType.FUNCTION) for var in self.symbols)
 
     def __str__(self):
         data = ""
         for symbol in self.symbols:
-            data+= symbol.__str__()
+            data += symbol.__str__()
         return f"""{{"SymbolTable": {data}}}"""
