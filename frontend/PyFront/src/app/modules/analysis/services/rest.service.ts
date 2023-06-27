6cb6c8185c554f5bf53c86996e983dcfda2100ea
Levi <levihernandez2501@gmail.com>
2023-06-27 02:20:36 -0600

    agregando url a service

--- frontend/PyFront/src/app/modules/analysis/services/rest.service.ts
+++ frontend/PyFront/src/app/modules/analysis/services/rest.service.ts
@@ -8,7 +8,7 @@ import {Observable} from "rxjs";
 
 export class RestService {
 
-  private readonly URL: string = 'http://localhost:8000/';
+  private readonly URL: string = 'http://34.148.56.163:8000/';
   constructor( private httpClient: HttpClient ) { }
 
   public post(body:any):Observable<any>{
